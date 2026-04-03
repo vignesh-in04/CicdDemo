35ecfb9b801a94f7536c97f3524cbfc29755af47
vignesh-in04 <vigneshprabhuvp44@gmail.com>
2026-04-03 14:25:39 +0530

    convert to real app with fast api

--- app/main.py
+++ app/main.py
@@ -1,5 +1,17 @@
-def add(a, b):
-    return a + b
+from fastapi import FastAPI
+import redis
+import os
 
-if __name__ == "__main__":
-    print("Sum:", add(2, 3))
+app = FastAPI()
+
+# Redis connection
+redis_host = os.getenv("REDIS_HOST", "redis")
+redis_port = int(os.getenv("REDIS_PORT", 6379))
+
+r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
+
+@app.get("/")
+def read_root():
+    r.incr("counter")
+    count = r.get("counter")
+    return {"message": "Hello from CI/CD 🚀", "visits": count}
